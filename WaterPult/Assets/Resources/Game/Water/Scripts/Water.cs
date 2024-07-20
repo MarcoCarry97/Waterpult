e39acf75f811ce7d7f59dcaba1b8f27c0c2ccaa6
MSI\marco <marcocarega.dev@gmail.com>
2024-07-20 23:01:33 +0200

    implemented Water class

--- WaterPult/Assets/Resources/Game/Water/Scripts/Water.cs
+++ WaterPult/Assets/Resources/Game/Water/Scripts/Water.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class Water : MonoBehaviour
 {
-    
+    [Range(1.0f, 20.0f)]
+    [SerializeField]
+    private float stayTime;
+
+    private bool destroyCondition=false;
+
+    private void Update()
+    {
+        if(destroyCondition)
+            Destroy(this.gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("VaseGround"))
+            destroyCondition = true;
+        else StartCoroutine(WaitingForDestroy());
+    }
+
+    private IEnumerator WaitingForDestroy()
+    {
+        yield return new WaitForSecondsRealtime(stayTime);
+        destroyCondition = true;
+    }
+
+
 }
